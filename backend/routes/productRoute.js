e2c0bd260e376c7c025962c3af7c407684ae9a19
tanveerisonline <tanveerisonline@gmail.com>
2023-05-03 00:49:55 +0530

    created first route

---
+++ backend/routes/productRoute.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const { getAllProducts } = require("../controller/productController");
+
+const router = express.Router();
+
+router.route("/products").get(getAllProducts)
+
+module.exports = router;
